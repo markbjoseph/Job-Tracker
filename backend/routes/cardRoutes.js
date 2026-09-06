cdb0ba379fa5f93bd3ffabafaa9a9aae98a19d1b
Mark Barce <mjosephbarce@gmail.com>
2026-09-06 13:34:42 +0100

    added card creation functionality

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const getCards = require("../controllers/cardController");
+const {getCards, createCards} = require("../controllers/cardController");
 const authToken = require("../middleware/authToken");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/cards", authToken, authMiddleware, getCards);
 
-router.post("/cards", authToken, authMiddleware, (req, res) => {
-    res.send("Card created");
-});
+router.post("/cards", authToken, authMiddleware, createCards);
 
 router.put("/cards/:id", authToken, authMiddleware, (req, res) => {
     res.send(`Update card ${req.params.id}`);
--- frontend/src/board.jsx
+++ frontend/src/board.jsx
@@ -23,6 +23,18 @@
     const [selectedCard, setSelectedCard] = useState(null);
 
     const [showTextList, setShowTextList] = useState(false);
+
+    const [editingList, setEditingList] = useState(false);
+    const [selectedList, setSelectedList] = useState(false);
+
+    const [listTitle, setListTitle] = useState(false);
+
+    const [addCardModal, setAddCardModal] = useState(false);
+
+    const [newCardTitle, setNewCardTitle] = useState(false);
+
+    const [newCardDescription, setNewCardDescription] = useState(false);
+
     // ----------------------------------------------------------------------------
 
     const userBoards = async (e) => {
@@ -64,6 +76,19 @@
         console.log(data);
     };
 
+    const createCard = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:3000/cards", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json" 
+            }, 
+            body: JSON.stringify({title: newCardTitle, description: newCardDescription, listId: selectedList})
+        });
+    }
+
     const updateBoard = async (e) => {
         e.preventDefault();
 
@@ -83,7 +108,31 @@
         console.log(data);
     };
 
-    
+    const updateList = async (e) => {
+
+        const response = await fetch(`http://localhost:3000/lists/${editingList}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({title: listTitle})
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+            setLists((currentLists) =>
+                currentLists.map((list) =>
+                    list.id === editingList
+            ? { ...list, title: listTitle }
+            : list
+        )
+    );
+
+    setEditingList(null);
+        
+    }
 
     const getList = async (board) => {
         
@@ -272,7 +321,25 @@
 
                         <div className="list" key={list.id}>
 
-                            <h3>{list.title}</h3>
+                            {editingList === list.id ? (
+                                <input
+                                    type="text"
+                                    value={listTitle}
+                                    onChange={(e) => setListTitle(e.target.value)}
+                                    onBlur={updateList}
+                                    autoFocus
+                                />
+
+                            ) : (
+                                <h3
+                                    onClick={() => {
+                                        setEditingList(list.id);
+                                        setListTitle(list.title);
+                                    }}
+                                    >
+                                        {list.title}
+                                    </h3>
+                            )}
 
                             <div className= "cards-container">
                                 {list.cards.map(card => (
@@ -288,7 +355,12 @@
 
                             ))}</div>
                         
-                            <button className="add-card">
+                            <button 
+                            className="add-card"
+                            onClick={() => {
+                                setAddCardModal(true)
+                                setSelectedList(list.id)
+                            }}>
                                 Add a Card + 
                             </button>
 
@@ -331,6 +403,35 @@
                 <h2>{selectedCard ? selectedCard.title : 'Card Details'}</h2>
                 <p>{selectedCard ? selectedCard.description : 'Card information goes here.'}</p>
                 <button onClick={() => setShowCardModal(false)}>Close</button>
+            </div>
+        </div>
+    )}
+
+    {addCardModal && (
+        <div className="modal-overlay">
+            <div className="modal">
+
+                <form onSubmit={createCard}>
+                
+                    <input 
+                    type="text"
+                    value={newCardTitle}
+                    onChange={(e) => setNewCardTitle(e.target.value)}
+                    placeholder="Enter a Title"
+                    />          
+
+                    <input 
+                    type="text"
+                    value={newCardDescription}
+                    onChange={(e) => setNewCardDescription(e.target.value)}
+                    placeholder="Enter a Description"
+                    />  
+
+                    <button type="submit">
+                        Add
+                    </button>
+
+                </form>      
             </div>
         </div>
     )}
